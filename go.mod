81f862a1102e20b99c54abc45c0e3f34cc4ee5e8
saptarshi dutta <saptarshidutta599@gmail.com>
2024-03-17 11:41:33 +0530

    mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sinore69/go
+
+go 1.22.1
+
+require github.com/sinore69/go-packages v1.0.0 // indirect
--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/sinore69/go-packages"
+	)
 
 func main() {
+	i := 0
+	for ; i < 10; i++ {
+		fmt.Print(i)
+		if i == 5 {
+			continue
+
+		}
+	}
 	fmt.Println()
+	fmt.Print(i)
+	j:=gopackages.Print()
+	fmt.Print(j)
 }
